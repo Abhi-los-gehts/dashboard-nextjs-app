84775bda787c82a171db560b40b69e64f0c4c2d2
Abhilasha <Abhilasha.reva61@gmail.com>
2025-02-10 19:25:03 +0100

    Added Tailwind CSS for global styling

--- interactive_dashboard/src/app/home/page.tsx
+++ interactive_dashboard/src/app/home/page.tsx
@@ -5,6 +5,13 @@ export default function home() {
       
       <h1> This is our home page</h1>
 
+ 
+     {/* To test if Tailwind CSS is working or no: its working */}
+      <h1 className="text-3xl bg-blue  font-bold underline">
+      Hello world!
+    </h1>
+
+
     </main>
 
 
